5705290baa1198a45fa099f25f0a420785ae7e9e
korgat <workkorgat@gmail.com>
2021-08-17 15:38:31 +0300

    added about, 404, setting pages

--- app/js/main.js
+++ app/js/main.js
@@ -25,10 +25,10 @@ $(function () {
 		$(".header__panel").toggleClass("header__panel-on")
 	})
 
-	$('.list__inner .tab').on('click', function (event) {
+	$('.list__inner .tab, .common__tabs .tab').on('click', function (event) {
 		var id = $(this).attr('data-id');
-		$('.list__inner').find('.tab-item').removeClass('active-tab').hide();
-		$('.list__inner .tabs').find('.tab').removeClass('active');
+		$('.list__inner, .common__tabs').find('.tab-item').removeClass('active-tab').hide();
+		$('.list__inner .tabs , .common__tabs .tabs').find('.tab').removeClass('active');
 		$(this).addClass('active');
 		$('#' + id).addClass('active-tab').fadeIn();
 		return false;
